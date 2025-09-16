ebc49e4505766eb4ae346701a6f234965110489b
ecarbonnel <ervael.carbonnel@gmail.com>
2025-09-16 08:04:47 -0500

    bind mount

--- TP4/run_app2.sh
+++ TP4/run_app2.sh
@@ -2,5 +2,5 @@ docker run -d \
   --name tp4-app2 \
   --network net-tp4 \
   -p 5001:5000 \
-  --mount type=bind,source=/srv/,target=/srv/ \
+  --mount type=bind,source=/home/user/tp-r504/TP4/srv/,target=/srv/ \
   im2-tp4
